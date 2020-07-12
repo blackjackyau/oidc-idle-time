6bc2f10d95525f730d6732a7a9dfaa029a75c05a
blackjackyau <yau.yik.shiung@gmail.com>
2020-07-12 20:54:03 +0800

    fix relative path issue

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ platformBrowserDynamic().bootstrapModule(AppModule)
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/sw.js')
+      .register('sw.js')
       .then(reg => console.log('Service Worker: Registered (Pages)'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
   });
